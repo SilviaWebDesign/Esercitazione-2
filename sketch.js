8bbedbc116ba60bdd958f51c86b7ba392cf220a2
SilviaWebDesign <silvia.lamastra@mail.polimi.it>
2025-10-06 18:08:05 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 // creo 100 particles
 let particles = []; let num = 10000;
-let m = 1; let n = 5; let threshold = 0.05;
+let m = 16; let n = 18; let threshold = 0.05;
+let minMN = 1; let maxMN = 6;
+let changePattern = true;
 
 function setup() {
   createCanvas(1550,725);
@@ -12,6 +14,10 @@ function setup() {
 
 function draw() {
   background(0);
+
+  if (changePattern) {
+    randomPatterns();
+  }
   
   for (let i=0; i<particles.length; i++){
     particles[i].update();
@@ -24,4 +30,19 @@ function chladni(x, y) {
   let L = 1;
   return cos(n * PI * x / L) * cos(m * PI * y / L) -
          cos(m * PI * x / L) * cos(n * PI * y / L);
+}
+
+function randomPatterns() {
+  m = floor(random(minMN, maxMN));
+  n = floor(random(minMN, maxMN));
+  
+  if (m === n) {
+    m = floor(random(minMN, maxMN));
+  }
+  
+  changePattern = false;
+}
+
+function mousePressed() {
+  changePattern = true; 
 }
