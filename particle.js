8fe9225e63c6ab3da00f04773edfa7220e061d75
SilviaWebDesign <silvia.lamastra@mail.polimi.it>
2025-10-06 16:52:50 +0200

    aggiunto particelle

---
+++ particle.js
@@ -0,0 +1,82 @@
+//creo la classe Particle
+class Particle {
+  constructor() {
+    this.position = createVector(random(0, width), random(0, height));
+//vettore con direzioni random con magnitudine 1
+    this.velocity = p5.Vector.random2D();
+    this.acceleration = createVector();
+    
+    this.maxSpeed = 50;
+    this.maxForce = 1;
+  }
+//creo dei confini
+  edges() {
+// se la posizione delle particelle supera la larghezza
+// riportale alla posizione 0
+// se invece è < 0 impostala uguale alla largezza
+    
+    if (this.position.x > width) {
+      this.position.x = 0;
+    } else if (this.position.x < 0) {
+      this.position.x = width;
+    }
+    
+     if (this.position.y > height) {
+      this.position.y = 0;
+    } else if (this.position.y < 0) {
+      this.position.x = height;
+    }
+  }
+
+//sistemo la velocità che desidero 
+//in base al disegno creato
+  seek() {
+    let x = map(this.position.x, 0, width, 0, 1);
+    let y = map(this.position.y, 0, height, 0, 1);
+// creo una variabile equal to chladni function
+// map tra 0 e 255 e riempio i riquadri in base a val
+      //let val = map(chladni(x, y), -2, 1, 0, 255);
+    let val = chladni(x, y);
+    
+    let target = this.position.copy();
+    
+// se val è superiore a 0 allora sarà colorato di nero
+// altrimenti sarà bianco
+// in questo modo ad ognii frame le particelle
+// si muoveranno sempre piu vicine alle linee nodali
+    if (abs(val) > threshold) {
+      target.x += random(-3, 3);
+      target.y += random(-3, 3);
+      
+      } 
+      
+    let desired = p5.Vector.sub(target, this.position);
+    desired.setMag(this.maxSpeed);
+    let steering = p5.Vector.sub(desired, this.velocity);
+    steering.limit(this.maxForce);
+    
+//  abbiamo definito l'accellerazione con cui il target si muove
+    
+    return steering;
+        
+  }
+  
+//metto in movimento le particelle
+  update(){
+    this.edges();
+    
+    this.acceleration.add(this.seek());
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(this.maxSpeed);
+    this.position.add(this.velocity);
+    this.acceleration.mult(0);
+  }
+  
+// disegno la particella
+  display() {
+    stroke(255);
+    point(this.position.x, this.position.y);
+  }
+  
+ 
+}
--- sketch.js
+++ sketch.js
@@ -1,47 +1,25 @@
-//definisco le variabili cols e rows per x e y
-let cols, rows; let size = 1;
-// modificando questi valori ottengo più combinazioni
-// threshold non è 0 per sistemare eventuali errori
-// che possono crearsi nell'esecuzione
+// creo 100 particles
+let particles = []; let num = 10000;
 let m = 1; let n = 5; let threshold = 0.05;
 
 function setup() {
-  createCanvas(1600, 1000);
-  cols = width/size;
-  rows = height/size;
+  createCanvas(1550,725);
+  for (let i=0; i<num; i++){
+    particles.push(new Particle());
+  } 
+  
 }
 
 function draw() {
-  background(220);
-  noStroke();
-
-// definisco due loop per i e j
-  for (let i=0; i<cols; i++) {
-    for (let j=0; j<rows; j++) {
-// mappo x e y per avere un range
-// verifico il range tramite print
-      let x = map(i, 0, cols, 0, 1);
-      let y = map(j, 0, rows, 0, 1);
-// creo una variabile equal to chladni function
-// map tra 0 e 255 e riempio i riquadri in base a val
-      //let val = map(chladni(x, y), -2, 1, 0, 255);
-      let val = chladni(x, y);
-// se val è inferiore a 0 allora sarà colorato di nero
-// altrimenti sarà bianco
-      if (abs(val) < threshold) {
-        fill(0);
-      } else {
-        fill(255);
-      }
-// creo una griglia di quadrati tramite rect
-      rect(i*size, j*size, size, size);
-    }
+  background(0);
+  
+  for (let i=0; i<particles.length; i++){
+    particles[i].update();
+    particles[i].display();
   }
-// static 
-  noLoop();
+   
 }
 
-// definisco la funzione chaldni e inserisco l'equazione omonima
 function chladni(x, y) {
   let L = 1;
   return cos(n * PI * x / L) * cos(m * PI * y / L) -
